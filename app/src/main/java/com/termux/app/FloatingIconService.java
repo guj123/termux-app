95ee211c072ac167c806c4063f79b352c86626d0
guj123 <guj565646@gmail.com>
2026-02-28 20:51:53 +0500

    Update FloatingIconService.java

--- app/src/main/java/com/termux/app/FloatingIconService.java
+++ app/src/main/java/com/termux/app/FloatingIconService.java
@@ -38,7 +38,7 @@ public class FloatingIconService extends Service {
         floatingIcon.setElevation(10f);
 
         // Set size to a standard app icon size (around 60dp)
-        int iconSize = (int) (10 * getResources().getDisplayMetrics().density);
+        int iconSize = (int) (30 * getResources().getDisplayMetrics().density);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             iconParams = new WindowManager.LayoutParams(
@@ -133,4 +133,5 @@ public class FloatingIconService extends Service {
         super.onDestroy();
         if (floatingIcon != null) windowManager.removeView(floatingIcon);
     }
+
 }
